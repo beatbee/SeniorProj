70f15ea0fd7579236c83ad0e1553fb7d637b6900
beatbee <bellbi123@gmail.com>
2024-10-17 16:21:59 +0700

    tets

--- app.js
+++ app.js
@@ -3,22 +3,33 @@ const express = require('express');
 const createError = require('http-errors');
 const logger = require('morgan');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const app = express();
+
 // Middleware
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const locationRouter = require('./routes/user_location/route');
 
+app.use(cookieParser());
 var compression = require('compression');
 app.use(compression());
+const authenticateToken = require('./middleware/authenticateToken');
+const locationRouter = require('./routes/user_location/route');
+const authRouter = require('./routes/auth/route');
 //route
 app.use('/location', locationRouter);
+app.use('/auth', authRouter);
 //app.use(restrictMiddleware)
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+//authentication
+app.get('/protected-route', authenticateToken, (req, res) => {
+    res.send('This is a protected route');
+});
+
 app.use(function (req, res, next) {
     next(createError(404));
 });
